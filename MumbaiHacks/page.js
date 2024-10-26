8a7a3d8c8081aadb2d4742ee04ae923d4b776240
Vansh Mehta <134204122+13vanshmehta@users.noreply.github.com>
2024-10-26 14:29:23 +0530

    Create page.js

---
+++ MumbaiHacks/page.js
@@ -0,0 +1,10 @@
+"use client";
+import StoreRecommendations from '../components/StoreRecommendations';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-8">
+      <StoreRecommendations />
+    </main>
+  );
+}
